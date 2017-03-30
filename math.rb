b752c7ed994e5e455b1e41cf1b6605836a98d2bb
Jordan Lui <jordan.lui@gmail.com>
2017-03-30 08:25:32 +0200

    math file

---
+++ math.rb
@@ -0,0 +1,26 @@
+a = 5
+
+while a <9
+	puts a
+	a +=1
+end
+
+b=2
+b *= 2 while b < 100
+puts b
+
+if b == 128
+	puts 'confirming that b is '
+	puts b
+end
+
+# Triple ===
+if /sera/ === "coursera"
+	puts "partial match"
+end
+
+if Integer === 21
+	puts "yes this is an integer"
+end
+
+# Case expressions
