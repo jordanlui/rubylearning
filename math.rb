906c2ac06357af67f313dca4e7ac1b5881c750f9
Jordan Lui <jordan.lui@gmail.com>
2017-04-01 21:35:02 +0200

    hashes learning

--- math.rb
+++ math.rb
@@ -39,3 +39,56 @@ location = 'Via Giuseppe, Rome,Italy'
 case location
 	when /rome/ then puts 'when in rome'
 end
+
+# Ranges
+some_range = 1..3
+range_exclusive = 1...10
+p ('k'...'z').to_a.sample(2)
+
+age = 26
+case age
+when 0..19 then puts "young person"
+when 20..99 then puts "you are older"
+else puts "you are very old"
+end
+
+# hashes
+# Also known as associated arrays
+# Make a hash
+# Index collections. Similar to regular arrays.
+editor_props = {"font" => "Arial", "size" => 12, "color" => "red"}
+
+puts editor_props.length
+puts editor_props['font']
+
+editor_props["background"] = "Blue"
+editor_props.each_pair do |key, value|
+	puts "Key: #{key} value: #{value}"
+end
+
+
+# Word frequency fast example
+word_frequency = Hash.new(0)
+
+sentence = "Chicka chicka boom boom in my room"
+sentence.split.each do |word|
+	word_frequency[word.downcase] += 1
+end
+
+puts word_frequency
+
+# Hash addressing
+
+def adjust_color (props = {foreground:"red", background: "white"})
+	puts "Foreground: #{props[:foreground]}" if props[:foreground]
+	puts "Background: #{props[:background]}" if props[:background]
+end
+
+adjust_color
+
+adjust_color background: "yella" # newly supported in Ruby 1.9. JSON style
+adjust_color ({:foreground => "green"})
+adjust_color foreground: "hi"
+
+# Hash block confusion testing
+puts one:"one"
